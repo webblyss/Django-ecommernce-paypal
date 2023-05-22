948045d4aac43c6bf3c299605b7d4b12f21a3b53
webblyss <skyhightech93@gmail.com>
2023-05-22 18:30:42 +0000

    cart quatity updated

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import cart,delete_cart
+from .views import cart,delete_cart,add_quantity,decrease_quantity
 
 
 
 app_name = "cart"
 urlpatterns = [
         path('',cart,name='cart'),
-        path('delete_cart/<int:product_id>/',delete_cart,name='delete')
+        path('delete_cart/<int:product_id>/',delete_cart,name='delete_cart'),
+        path('add_qauntity/<int:product_id>/',add_quantity,name="add_qauntity"),
+        path('decrease_quantity/<int:product_id>/',decrease_quantity,name="decrease_quantity")
 ]
 
 
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import CartItem
 from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 # Create your views here.
 
 
@@ -28,17 +30,27 @@ def delete_cart(request,product_id):
     cart_item = get_object_or_404(CartItem, id=product_id)
     cart_item.delete()
     print('item deleted successfully')
-    return redirect('cart')
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
-def addQuantity(request,pk):
+def add_quantity(request, product_id):
+    cart_item = get_object_or_404(CartItem, id=product_id)
     
-    cart = CartItem.objects.get(pk=pk)                                    
+    # Update the quantity
+    cart_item.quantity += 1 
+    cart_item.save()
     
-    
-    
-    
-    return HttpResponseRedirect('')
+    print("Cart quantity updated successfully")    
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 
+def decrease_quantity(request,product_id):
+    cart_item = get_object_or_404(CartItem, id=product_id)
+    # Update the quantity
+    cart_item.quantity -= 1 
+    cart_item.save()
+    print("Cart quantity updated successfully")   
+    return redirect(request.META.get('HTTP_REFERER'))
+    
+    
